2e889e9343853b5af1b1b4574be360d3f2528cc9
ChaeMoon <jungchaemoon@gmail.com>
2019-08-29 15:28:40 +0900

    Added url

--- analysis/urls.py
+++ analysis/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path, include
 from rest_framework import routers
-from .views import DataAnalysisView
+from .views import DataSaveView, GraphView
 
 app_name = 'analysis'
 
 router = routers.DefaultRouter()
-router.register('data', DataAnalysisView)
+router.register('data_save', DataSaveView)
 
 urlpatterns = [
-    path('', include(router.urls))
+    path('', include(router.urls)),
+    path('graph/', GraphView.as_view(), name='graph'),
+
 ]
