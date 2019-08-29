4ea818d46ce4a15898d21b0d3f5188b28b6e3253
ChaeMoon <jungchaemoon@gmail.com>
2019-08-29 15:28:09 +0900

    Updated datasaveview, graphview

--- analysis/views.py
+++ analysis/views.py
@@ -1,12 +1,22 @@
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.views.generic import TemplateView
 from rest_framework import viewsets
-from .serializers import DataSerializer
 from .models import Data
+from .serializers import DataSerializer
 
 
-class DataAnalysisView(viewsets.ModelViewSet):
+class DataSaveView(viewsets.ModelViewSet):
     queryset = Data.objects.all()
     serializer_class = DataSerializer
 
-    def post(self, request):
-        self.serializer_class(data=request.data)
+    def create(self, request, *args, **kwargs):
+        response = super(DataSaveView, self).create(request, *args, **kwargs)
+        return HttpResponseRedirect(reverse('data_list'))
 
+
+class GraphView(TemplateView):
+    template_name = 'analysis/graph.html'
+
+    def get(self, request, *args, **kwargs):
+        return super(GraphView, self).get(request, *args, **kwargs)
