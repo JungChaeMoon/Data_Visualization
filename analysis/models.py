2c9cb349591fd5f40eb7100b3239d817f728177b
JungChaeMoon <jungchaemoon@gmail.com>
2019-09-17 17:34:34 +0900

    Updated database modeling

--- analysis/models.py
+++ analysis/models.py
@@ -1,15 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class DataModel(models.Model):
+    name = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.name
 
 
-class DataModel1(models.Model):
-    weight = models.IntegerField()
-    gyro = models.IntegerField()
-    timestamp = models.DateTimeField(auto_now_add=True)
-
-
-class DataModel2(models.Model):
+class DataSaveModel(models.Model):
+    data_model = models.ForeignKey('analysis.DataModel', on_delete=models.CASCADE, related_name='data_save_model')
     weight = models.IntegerField()
     gyro = models.IntegerField()
     timestamp = models.DateTimeField(auto_now_add=True)
