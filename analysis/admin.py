57aeaf77e41e5c5d1d125bcccd2f1c6c93a07909
JungChaeMoon <jungchaemoon@gmail.com>
2019-09-17 17:21:55 +0900

    Updated admin page

--- analysis/admin.py
+++ analysis/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from .models import DataModel1, DataModel2
+from .models import DataModel, DataSaveModel
 
-
-admin.site.register(DataModel1)
-admin.site.register(DataModel2)
+admin.site.register(DataModel)
+admin.site.register(DataSaveModel)
