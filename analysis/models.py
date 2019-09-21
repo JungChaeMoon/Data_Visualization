3ae18354280853f6a4985b5b90d9cef8c752d8aa
JungChaeMoon <jungchaemoon@gmail.com>
2019-09-21 23:29:43 +0900

    Added accel column

--- analysis/models.py
+++ analysis/models.py
@@ -12,4 +12,7 @@ class DataSaveModel(models.Model):
     data_model = models.ForeignKey('analysis.DataModel', on_delete=models.CASCADE, related_name='data_save_model')
     weight = models.IntegerField()
     gyro = models.IntegerField()
+    accelx = models.IntegerField(null=True, blank=True)
+    accely = models.IntegerField(null=True, blank=True)
+    accelz = models.IntegerField(null=True, blank=True)
     timestamp = models.DateTimeField(auto_now_add=True)
