7c0d9fb1a4030f3c363f2d27f85947ec453efced
JungChaeMoon <jungchaemoon@gmail.com>
2019-09-22 19:16:20 +0900

    Updated graph page

--- analysis/views.py
+++ analysis/views.py
@@ -24,14 +24,36 @@ class GraphView(APIView):
 
     def get(self, request, *args, **kwargs):
         weight1 = DataSaveModel.objects.filter(data_model=1).values_list('weight').order_by()[:1]
-        first_weight = DataSaveModel.objects.filter(data_model=1).values_list('weight')
+        first_weight1 = DataSaveModel.objects.filter(data_model=1).values_list('weight')
+        first_weight2 = DataSaveModel.objects.filter(data_model=2).values_list('weight')
+        first_weight3 = DataSaveModel.objects.filter(data_model=3).values_list('weight')
         timestamp1 = DataSaveModel.objects.filter(data_model=1).values_list('timestamp').order_by()[:30]
         weight2 = DataSaveModel.objects.filter(data_model=2).values_list('weight').order_by()[:30]
         timestamp2 = DataSaveModel.objects.filter(data_model=2).values_list('timestamp').order_by()[:30]
+        weight3 = DataSaveModel.objects.filter(data_model=3).values_list('weight').order_by()[:1]
+        timestamp3 = DataSaveModel.objects.filter(data_model=3).values_list('timestamp').order_by()[:30]
+        gyro1x = DataSaveModel.objects.filter(data_model=1).values_list('gyrox')
+        gyro1y = DataSaveModel.objects.filter(data_model=1).values_list('gyroy')
+        gyro1z = DataSaveModel.objects.filter(data_model=1).values_list('gyroz')
+        gyro2x = DataSaveModel.objects.filter(data_model=2).values_list('gyrox')
+        gyro2y = DataSaveModel.objects.filter(data_model=2).values_list('gyroy')
+        gyro2z = DataSaveModel.objects.filter(data_model=2).values_list('gyroz')
+        gyro3x = DataSaveModel.objects.filter(data_model=3).values_list('gyrox')
+        gyro3y = DataSaveModel.objects.filter(data_model=3).values_list('gyroy')
+        gyro3z = DataSaveModel.objects.filter(data_model=3).values_list('gyroz')
+        accel1x = DataSaveModel.objects.filter(data_model=1).values_list('accelx')
+        accel1y = DataSaveModel.objects.filter(data_model=1).values_list('accely')
+        accel1z = DataSaveModel.objects.filter(data_model=1).values_list('accelz')
+        accel2x = DataSaveModel.objects.filter(data_model=2).values_list('accelx')
+        accel2y = DataSaveModel.objects.filter(data_model=2).values_list('accely')
+        accel2z = DataSaveModel.objects.filter(data_model=2).values_list('accelz')
+        accel3x = DataSaveModel.objects.filter(data_model=3).values_list('accelx')
+        accel3y = DataSaveModel.objects.filter(data_model=3).values_list('accely')
+        accel3z = DataSaveModel.objects.filter(data_model=3).values_list('accelz')
 
         data = {
             'weight1': weight1,
-            'first_weight': first_weight,
+            'first_weight1': first_weight1,
             'timestamp1': timestamp1,
             'weight2': weight2,
             'timestamp2': timestamp2,
